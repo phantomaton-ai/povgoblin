e7f77e5968fcee1c945ef258cbe44de70e09e001
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-22 12:50:08 -0700

    extraneous

--- povgoblin.js
+++ povgoblin.js
@@ -3,5 +3,5 @@ import phantomaton from 'phantomaton';
 
 export default function povgoblin(configuration = {}) {
   const sysprompt = fs.readFileSync('povgoblin.md', 'utf-8');
-  return phantomaton(sysprompt, process.cwd(), configuration);
+  return phantomaton(sysprompt, process.cwd());
 }
