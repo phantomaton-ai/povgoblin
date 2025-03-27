72059bb32254e148497c03f3d5f999d2234df38e
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-27 13:41:15 -0700

    Fix issue loading promptfile after install

--- povgoblin.js
+++ povgoblin.js
@@ -1,10 +1,16 @@
 import fs from 'fs';
+import path from 'path';
 import phantomaton from 'phantomaton';
+import { fileURLToPath } from 'url';
 
 import plugin from './plugin.js';
 
-export default function povgoblin(request, options = {}) {
-  const sysprompt = fs.readFileSync('povgoblin.md', 'utf-8');
-  const install = [plugin({ ...options, request })];
-  return phantomaton(sysprompt, { install });
-}
+const promptfile = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  'povgoblin.md'
+);
+
+export default (request, options = {}) => phantomaton(
+  fs.readFileSync(promptfile, 'utf-8'),
+  { install: [plugin({ ...options, request })] }
+);
